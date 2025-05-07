f510021237142f6521b1e3a207adefe8ee7ba94b
Guido Perman <guidoyaelperman@gmail.com>
2025-05-07 12:36:04 -0300

    refactored layout component and moved navbar section to specific component

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { AsideComponent } from '../components/aside/aside.component';
+import { NavbarComponent } from "../components/navbar/navbar.component";
 
 @Component({
   selector: 'app-layout',
-  imports: [RouterOutlet, AsideComponent, RouterLink],
+  imports: [RouterOutlet, AsideComponent, NavbarComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
