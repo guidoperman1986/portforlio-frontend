6ae0ed4f49e76e2c74cba50929f1dd2a9b362332
Guido Perman <guidoyaelperman@gmail.com>
2025-05-07 14:25:30 -0300

    feat: enhance navbar with dynamic page titles and improve routing links

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,10 +5,26 @@ export const routes: Routes = [
         path: '',
         loadComponent: () => import('./layout/layout.component').then(c => c.LayoutComponent),
         children: [
-            { path: 'about', loadComponent: () => import('./pages/about/about.component').then(c => c.AboutComponent) },
-            { path: 'resume', loadComponent: () => import('./pages/resume/resume.component').then(c => c.ResumeComponent) },
-            { path: 'portfolio', loadComponent: () => import('./pages/portfolio/portfolio.component').then(c => c.PortfolioComponent) },
-            { path: 'contact', loadComponent: () => import('./pages/contact/contact.component').then(c => c.ContactComponent) },
+            {
+                path: 'about',
+                loadComponent: () => import('./pages/about/about.component').then(c => c.AboutComponent),
+                data: { title: 'About Me' },
+            },
+            {
+                path: 'resume',
+                loadComponent: () => import('./pages/resume/resume.component').then(c => c.ResumeComponent),
+                data: { title: 'Resume' },
+            },
+            {
+                path: 'portfolio',
+                loadComponent: () => import('./pages/portfolio/portfolio.component').then(c => c.PortfolioComponent),
+                data: { title: 'Portfolio' },
+            },
+            {
+                path: 'contact',
+                loadComponent: () => import('./pages/contact/contact.component').then(c => c.ContactComponent),
+                data: { title: 'Contact' },
+            },
         ]
     }
 ];
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { filter, map, tap } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  activatedRoute = inject(ActivatedRoute);
+  router = inject(Router);
+  pageTitle = signal<string>('');
+
+  ngOnInit(): void {
+    // Handle page reload by directly accessing the snapshot data
+    const initialTitle = this.activatedRoute.firstChild?.snapshot.data['title'];
+    if (initialTitle) {
+      this.pageTitle.set(initialTitle);
+    }
+
+    this.router.events
+      .pipe(
+        filter((event) => event instanceof NavigationEnd),
+        map(() => this.activatedRoute.firstChild?.snapshot.data['title'])
+      )
+      .subscribe(this.pageTitle.set);
+
+
+  }
 
 }
