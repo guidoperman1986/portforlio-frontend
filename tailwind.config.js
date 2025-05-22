ad064f91e18161bb5730007ec6c112df374363d2
Guido Perman <guidoyaelperman@gmail.com>
2025-05-22 14:42:19 -0300

    styles modified on aside, navbar, what-i-do and card

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,12 +8,15 @@ module.exports = {
                 'shadow-1': '0 16px 30px hsla(0, 0%, 0%, 0.125)',
             },
             backgroundColor: {
-                "onyx": "bg-[linear-gradient(to_bottom_right,_hsl(240,1%,20%)_3%,_hsl(0,0%,15%)_97%)]"    
+                "onyx": "bg-[linear-gradient(to_bottom_right,_hsl(240,1%,20%)_3%,_hsl(0,0%,15%)_97%)]",
             },
             backgroundImage: {
                 "gradient-onyx":
-                    "linear-gradient(to bottom right, hsl(240,1%,25%) 3%, hsl(0,0%,19%) 97%)",
+                "linear-gradient(to bottom right, hsl(240,1%,25%) 3%, hsl(0,0%,19%) 97%)",
             },
+            textColor: {
+                "onyx": "#FECE66"
+            }
         },
     },
     plugins: [],
